397bc3c514504dd62074476780d60a19a6def0a1
Thibault FORMAL <tformal@sensei.int.europe.naverlabs.com>
2024-06-07 14:36:09 +0200

    update retriever configs

--- models/retrievers/dense.py
+++ models/retrievers/dense.py
@@ -12,7 +12,7 @@ class Dense(Retriever):
 
     def __init__(self, model_name, max_len, pooler, similarity, prompt_q=None, prompt_d=None):
         self.model_name = model_name
-        self.model = AutoModel.from_pretrained(self.model_name, low_cpu_mem_usage=True,torch_dtype=torch.float16)
+        self.model = AutoModel.from_pretrained(self.model_name, low_cpu_mem_usage=True, torch_dtype=torch.float16, trust_remote_code=True)
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = self.model.to(self.device)
