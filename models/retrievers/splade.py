80dafdf0e927a347cc3383c4ad23d920fe2e5328
Thibault FORMAL <tformal@chaos-07.int.europe.naverlabs.com>
2024-06-12 10:33:50 +0200

    fix asymmetric splade and add eff-splade config

--- models/retrievers/splade.py
+++ models/retrievers/splade.py
@@ -10,19 +10,34 @@ from models.retrievers.retriever import Retriever
 
 
 class Splade(Retriever):
-    def __init__(self, model_name,max_len=512):
+    def __init__(self, model_name, max_len=512, query_encoder_name=None):
 
         self.model_name = model_name
         self.max_len = max_len
-        self.model = AutoModelForMaskedLM.from_pretrained(self.model_name, low_cpu_mem_usage=True,torch_dtype=torch.float16)
-        #self.model = torch.compile(self.model)
-        
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name,max_length=self.max_len)
-        self.model.eval()
+        self.model = AutoModelForMaskedLM.from_pretrained(self.model_name, low_cpu_mem_usage=True, torch_dtype=torch.float16)
+        if query_encoder_name:
+            self.query_encoder = AutoModelForMaskedLM.from_pretrained(query_encoder_name, torch_dtype=torch.float16, low_cpu_mem_usage=True)
+        else:
+            self.query_encoder = self.model  # otherwise symmetric
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, max_length=self.max_len)
         self.reverse_vocab = {v: k for k, v in self.tokenizer.vocab.items()}
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        if query_encoder_name:
+            self.query_encoder = self.query_encoder.to(self.device)
+            self.query_encoder.eval()
+        if torch.cuda.device_count() > 1 and torch.cuda.is_available():
+            self.model = torch.nn.DataParallel(self.model)
+            if query_encoder_name:
+                self.query_encoder = torch.nn.DataParallel(self.query_encoder)
         
-    def __call__(self, kwargs):
+    def __call__(self, query_or_doc, kwargs):
         kwargs = {key: value.to('cuda') for key, value in kwargs.items()}
+        if query_or_doc == "doc":
+            outputs = self.model(**kwargs)
+        else:  # query_or_doc == "query"
+            outputs = self.query_encoder(**kwargs)
         outputs = self.model(**kwargs).logits
 
         # pooling over hidden representations
