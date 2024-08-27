33f884dde3cc98eaf245044c484f637c0875ca38
Vassilina Nikoulina <vassilina.nikoulina@naverlabs.com>
2024-08-27 16:35:37 +0200

    fixed weird computation

--- models/evaluators/utils.py
+++ models/evaluators/utils.py
@@ -5,7 +5,7 @@ def process_llm_outputs_assess_scores(outputs, options, unknown_value=-100):
     
     possible_scores = [[options[opt] for opt in options if opt.lower() in rep.lower() ] for rep in outputs]
     scores = [sc[0] if len(sc)==1 else unknown_value for sc in possible_scores]
-    weird = [rep for i,rep in enumerate(outputs) if (len(possible_scores[i])==0 or len(possible_scores)>1)]
+    weird = [rep for i,rep in enumerate(outputs) if (len(possible_scores[i])==0 or len(possible_scores[i])>1)]
     return scores, weird
 
 def get_mean_without_unknown(scores, unknown_value=-100):
