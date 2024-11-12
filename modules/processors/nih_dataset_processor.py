4a79cf614e9cf16c4dce0f868b1f871d9b8ff13b
Stephane Clinchant <sclincha@tholus-24.int.europe.naverlabs.com>
2024-11-12 16:06:55 +0100

    nih HF dataset

--- modules/processors/nih_dataset_processor.py
+++ modules/processors/nih_dataset_processor.py
@@ -2,36 +2,58 @@ import datasets
 import os
 from ..dataset_processor import *
 
+class NIHDataset(Processor):
 
-class NIHJsonDataset(Processor):
-
-    def __init__(self, filepath,index=0, *args, **kwargs):
-        self.filepath=filepath
-        self.dataset_name = os.path.basename(filepath).split('.')[0]
-        self.index=index # index to extract query are 0, documents are 1
+    def __init__(self, config_name='number',is_query=True, *args, **kwargs):
+        self.dataset_name='nih_v1_'+config_name
+        if is_query:
+            self.dataset_name+='_query'
+        else:
+            self.dataset_name+='_doc'
+        self.conf_name= config_name
+        self.is_query=is_query
         
         super().__init__(*args, **kwargs, dataset_name=self.dataset_name)
     
     def process(self):
-        X=json.load(open(self.filepath))
-        #
-        data=[x[self.index] for x in X]
-        if self.index==0:
-            #reprocess single str label into a list of str
-            for y in data:
-                y['label']=[y['label']]
-        #oracle data
-        qids_dids_l =[ (x[0]['id'],x[1]['id']) for x in X]
-        #FIXME get the run dirs
+        print(self.conf_name)
+        d = datasets.load_dataset('naver/bergen_nih_v1',self.conf_name)[self.split]
+        #oracle run data generations
+        qids_dids_l =[ (x['qid'],x['did']) for x in d]
+        #FIXME get the run dirs?
         fname='runs/run.oracle.'+self.dataset_name+'.dev.trec'
         #6915606477668963399	q0	10593264_2	0	100	run
         f=open(fname,'w')
         for i in qids_dids_l:
             f.write(i[0]+'\tq0\t'+i[1]+'\t0\t100\trun\n')
         f.close()
-        return datasets.Dataset.from_list(data)
+        
+        if self.is_query:
+            #reprocess single str label into a list of str    
+            dataset = d.rename_column("qid", "id")
+            dataset = dataset.rename_column("query", "content")
+            dataset = dataset.remove_columns(['did', 'doc'])
+            dataset = dataset.map(lambda x: {"label": [str(x["label"])]})
+            print('QUERY:',dataset[:10])
+            return dataset
+        else:
+            #document dataset
+            dataset = d.rename_column("did", "id")
+            dataset = dataset.rename_column("doc", "content")
+            dataset = dataset.remove_columns(['qid', 'query','label'])
+            print(dataset[:10])
+            return dataset
 
-#FIXME upload dataset on HF easier 
-#Do the 3 configs done reclean the processor
-    
+class NIHDatasetNumber(NIHDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs,config_name='number')
+
+class NIHDatasetSimple(NIHDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs,config_name='simple')
+
+class NIHDatasetMultiHop(NIHDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs,config_name='multihop')
+
     
