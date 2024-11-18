7cee6b2f1fe0cd947685344dc903d71a7bebaddc
Thibault Formal <61324007+thibault-formal@users.noreply.github.com>
2024-11-18 12:03:36 +0100

    fix import

--- modules/rag.py
+++ modules/rag.py
@@ -8,7 +8,7 @@ from modules.retrieve import Retrieve
 from modules.rerank import Rerank
 from modules.generate_query import GenerateQueries
 from modules.process_context import ProcessContext
-frfrom modules.metrics import RAGMetrics
+from modules.metrics import RAGMetrics
 import time 
 import random
 import shutil
