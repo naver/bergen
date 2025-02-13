145b4de0e4f04c5109df154fc117b6bc8915d47d
Thibault FORMAL <tformal@sensei.int.europe.naverlabs.com>
2025-02-13 17:40:01 +0100

    modify ambigqa processor

--- modules/processors/ambig_qa_processor.py
+++ modules/processors/ambig_qa_processor.py
@@ -5,141 +5,212 @@ from collections import defaultdict
 import urllib.request
 
 
-# UB means Upper Bound for our experiments
-# FIXME rename
-class AmbigQA_UB(Processor):
+class AmbigQAQueries(Processor):
 
     def __init__(self, *args, **kwargs):
-        dataset_name = "ambig_qa_ub"
+        dataset_name = "ambigqa-queries"
         super().__init__(*args, **kwargs, dataset_name=dataset_name)
 
     def process(self):
-        # dataset = datasets.load_dataset(hf_name, hf_query_or_doc_name, num_proc=self.num_proc)[self.split]
-        # ds = load_dataset("sewon/ambig_qa", "light",num_proc=self.num_proc)['train']
-        def my_gen():
-            sid = "ambigqa_ub"
-            i = 0
-            # for split in ['train','dev','test']:
-            # we keep only dev and test for now
-            for split in ["dev", "test"]:
-                ds = load_dataset(
-                    "erbacher/AmbigNQ-clarifying-question", num_proc=self.num_proc
-                )[split]
-                for x in ds:
-                    # FIXME for some reason the fields are stored as string instead of list
-                    qs = eval(x["intent"])
-                    answers = eval(x["answer"])
-                    if x["ambig"] is True:
-                        for q, a in zip(qs, answers):
-                            qid = sid + str(i)
-                            i += 1
-                            # print(q,a)
-                            if q is not None and a is not None:
-                                yield {"id": qid, "content": q, "label": [a]}
-
-        return datasets.Dataset.from_generator(my_gen)
+        hf_name = "naver/ambigqa-queries"
+        dataset = load_dataset(hf_name, num_proc=self.num_proc)[self.split]
+        dataset = dataset.rename_column("question", "content")
+        return dataset
 
 
-class AmbigQAInstructV1(Processor):
-    """ """
+class AmbigQAInstructFromDict(Processor):
+    """basic class, instructions are contained in local json file
+    TOREMOVE when we push a dataset on HF
+    """
 
     def __init__(self, *args, **kwargs):
-        dataset_name = "ambig_qa_InstructV1"
+        dataset_name = "ambigqa-v3-long-cat2-25q"
         super().__init__(*args, **kwargs, dataset_name=dataset_name)
 
     def process(self):
         def my_gen():
-            i = 0
             # LOCAL FILE => to change after
-            file = json.load(
+            # data = json.load(
+            #     open(
+            #         "/beegfs/scratch/user/hdejean/bergen_branches_test/TAR/scripts/prompt_type3_v2_gpt4.json"
+            #     )
+            # )
+            # data = json.load(
+            #     open(
+            #         "/beegfs/scratch/user/hdejean/bergen_branches_test/TAR/scripts/prompt_type3_500q_gpt4.json"
+            #     )
+            # )  # type3_500q
+            # data = json.load(
+            #     open(
+            #         "/beegfs/scratch/user/hdejean/bergen_branches_test/TAR/scripts/prompt_Nadia_v1_categ2_500_gpt4.json"
+            #     )
+            # )
+            data = json.load(
                 open(
-                    "/beegfs/scratch/team/nlp/llm_hackathon/instruction_following_retrieval/data_/ambigqa_basic_queries_prompts_gpt4.json"
+                    "/beegfs/scratch/user/tformal/evian/save/generated_instructions_40_categ2_with-doc_medium-size_gpt4.json"
                 )
             )
+            hf_name = "naver/ambigqa-queries"
+            dataset = load_dataset(hf_name, num_proc=self.num_proc)[self.split]
             d = {}
-            for item in file:
+            for item in data:
                 key_ = list(item.keys())[0]
                 d[key_] = item[key_]
-            for split in ["dev", "test"]:
-                ds = load_dataset(
-                    "erbacher/AmbigNQ-clarifying-question", num_proc=self.num_proc
-                )[split]
-                for x in ds:
-                    if x["ambig"]:
-                        # FIXME for some reason the fields are stored as string instead of list
-                        q = x[
-                            "question"
-                        ]  # ambiguous question (to which we are going to append an instruction)
-                        answer = eval(x["answer"])[
-                            0
-                        ]  # hard-coded (bc how we constructed the instructions for now ==> the first answer is the correct one)
-                        try:
-                            instruction = d[f"ambigqa_{i}"]["instruction"]
-                        except KeyError:
-                            print(d[f"ambigqa_{i}"])
-                            instruction = ""
-                        # assert d[f"ambigqa_{i}"]["original query"] == q, print(
-                        #     d[f"ambigqa_{i}"]["original query"], q
-                        # )
-                        yield {
-                            "id": f"ambigqa_{i}",
-                            "content": q.strip(),
-                            "instruction": instruction.strip(),
-                            "label": [answer],
-                        }
-                        i += 1
+            for sample in dataset:
+                id_ = sample["id"]
+                if id_ in d:
+                    # q = sample["question"]
+                    q = sample["nq_question"]
+                    answer = sample["answer"]
+                    try:
+                        instruction = d[id_]["instruction"]
+                    except KeyError:
+                        print(id_)
+                        print(d[id_])
+                    yield {
+                        "id": id_,
+                        "content": q.strip(),
+                        "instruction": instruction.strip(),
+                        "label": [answer],
+                    }
 
         return datasets.Dataset.from_generator(my_gen)
 
 
-class AmbigQAInstructV2(Processor):
-    """ """
+class AmbigQAFromDict(Processor):
+    """temp, to remove"""
 
     def __init__(self, *args, **kwargs):
-        dataset_name = "ambig_qa_InstructV2"
+        dataset_name = "ambigqa-500q"
         super().__init__(*args, **kwargs, dataset_name=dataset_name)
 
     def process(self):
         def my_gen():
-            i = 0
-            # LOCAL FILE => to change after
-            file = json.load(
+            data = json.load(
                 open(
-                    "/beegfs/scratch/team/nlp/llm_hackathon/instruction_following_retrieval/data_/ambigqa_queries_with_docs_prompts_gpt4.json"
+                    "/beegfs/scratch/user/hdejean/bergen_branches_test/TAR/scripts/prompt_Nadia_v1_categ3_500_gpt4.json"
                 )
-            )
+            )  # only used to gather the same ids
+            hf_name = "naver/ambigqa-queries"
+            dataset = load_dataset(hf_name, num_proc=self.num_proc)[self.split]
             d = {}
-            for item in file:
+            for item in data:
                 key_ = list(item.keys())[0]
                 d[key_] = item[key_]
-            for split in ["dev", "test"]:
-                ds = load_dataset(
-                    "erbacher/AmbigNQ-clarifying-question", num_proc=self.num_proc
-                )[split]
-                for x in ds:
-                    if x["ambig"]:
-                        # FIXME for some reason the fields are stored as string instead of list
-                        q = x[
-                            "question"
-                        ]  # ambiguous question (to which we are going to append an instruction)
-                        answer = eval(x["answer"])[
-                            0
-                        ]  # hard-coded (bc how we constructed the instructions for now ==> the first answer is the correct one)
-                        try:
-                            instruction = d[f"ambigqa_ub{i}"]["instruction"]
-                        except KeyError:
-                            # print(d[f"ambigqa_ub{i}"])
-                            print(f"ambigqa_ub{i}")
-                            instruction = ""
-                        yield {
-                            "id": f"ambigqa_ub{i}",
-                            "content": q.strip(),
-                            "instruction": instruction.strip(),
-                            "label": [answer],
-                        }
-                        i += 1
+            for sample in dataset:
+                id_ = sample["id"]
+                if id_ in d:
+                    q = sample["question"]
+                    answer = sample["answer"]
+                    yield {
+                        "id": id_,
+                        "content": q.strip(),
+                        "label": [answer],
+                    }
 
         return datasets.Dataset.from_generator(my_gen)
+
+
+# class AmbigQAInstructV1(Processor):
+#     """ """
+
+#     def __init__(self, *args, **kwargs):
+#         dataset_name = "ambig_qa_InstructV1"
+#         super().__init__(*args, **kwargs, dataset_name=dataset_name)
+
+#     def process(self):
+#         def my_gen():
+#             i = 0
+#             # LOCAL FILE => to change after
+#             file = json.load(
+#                 open(
+#                     "/beegfs/scratch/team/nlp/llm_hackathon/instruction_following_retrieval/data_/ambigqa_basic_queries_prompts_gpt4.json"
+#                 )
+#             )
+#             d = {}
+#             for item in file:
+#                 key_ = list(item.keys())[0]
+#                 d[key_] = item[key_]
+#             for split in ["dev", "test"]:
+#                 ds = load_dataset(
+#                     "erbacher/AmbigNQ-clarifying-question", num_proc=self.num_proc
+#                 )[split]
+#                 for x in ds:
+#                     if x["ambig"]:
+#                         # FIXME for some reason the fields are stored as string instead of list
+#                         q = x[
+#                             "question"
+#                         ]  # ambiguous question (to which we are going to append an instruction)
+#                         answer = eval(x["answer"])[
+#                             0
+#                         ]  # hard-coded (bc how we constructed the instructions for now ==> the first answer is the correct one)
+#                         try:
+#                             instruction = d[f"ambigqa_{i}"]["instruction"]
+#                         except KeyError:
+#                             print(d[f"ambigqa_{i}"])
+#                             instruction = ""
+#                         # assert d[f"ambigqa_{i}"]["original query"] == q, print(
+#                         #     d[f"ambigqa_{i}"]["original query"], q
+#                         # )
+#                         yield {
+#                             "id": f"ambigqa_{i}",
+#                             "content": q.strip(),
+#                             "instruction": instruction.strip(),
+#                             "label": [answer],
+#                         }
+#                         i += 1
+
+#         return datasets.Dataset.from_generator(my_gen)
+
+
+# class AmbigQAInstructV2(Processor):
+#     """ """
+
+#     def __init__(self, *args, **kwargs):
+#         dataset_name = "ambig_qa_InstructV2"
+#         super().__init__(*args, **kwargs, dataset_name=dataset_name)
+
+#     def process(self):
+#         def my_gen():
+#             i = 0
+#             # LOCAL FILE => to change after
+#             file = json.load(
+#                 open(
+#                     "/beegfs/scratch/team/nlp/llm_hackathon/instruction_following_retrieval/data_/ambigqa_queries_with_docs_prompts_gpt4.json"
+#                 )
+#             )
+#             d = {}
+#             for item in file:
+#                 key_ = list(item.keys())[0]
+#                 d[key_] = item[key_]
+#             for split in ["dev", "test"]:
+#                 ds = load_dataset(
+#                     "erbacher/AmbigNQ-clarifying-question", num_proc=self.num_proc
+#                 )[split]
+#                 for x in ds:
+#                     if x["ambig"]:
+#                         # FIXME for some reason the fields are stored as string instead of list
+#                         q = x[
+#                             "question"
+#                         ]  # ambiguous question (to which we are going to append an instruction)
+#                         answer = eval(x["answer"])[
+#                             0
+#                         ]  # hard-coded (bc how we constructed the instructions for now ==> the first answer is the correct one)
+#                         try:
+#                             instruction = d[f"ambigqa_ub{i}"]["instruction"]
+#                         except KeyError:
+#                             # print(d[f"ambigqa_ub{i}"])
+#                             print(f"ambigqa_ub{i}")
+#                             instruction = ""
+#                         yield {
+#                             "id": f"ambigqa_ub{i}",
+#                             "content": q.strip(),
+#                             "instruction": instruction.strip(),
+#                             "label": [answer],
+#                         }
+#                         i += 1
+
+#         return datasets.Dataset.from_generator(my_gen)
 
 
 # class AmbigQABase(Processor):
